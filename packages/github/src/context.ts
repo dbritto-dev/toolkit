04f8d3d7cb68c13e0fb71309fc537a9ab1fe75cf
dbritto-dev <dbritto.dev@gmail.com>
2022-03-15 19:35:49 -0500

    add support for ref name, ref type and ref protected

--- packages/github/src/context.ts
+++ packages/github/src/context.ts
@@ -12,6 +12,9 @@ export class Context {
   eventName: string
   sha: string
   ref: string
+  refName: string
+  refType: string
+  refProtected
   workflow: string
   action: string
   actor: string
@@ -40,6 +43,9 @@ export class Context {
     this.eventName = process.env.GITHUB_EVENT_NAME as string
     this.sha = process.env.GITHUB_SHA as string
     this.ref = process.env.GITHUB_REF as string
+    this.refName = process.env.GITHUB_REF_NAME as string
+    this.refType = process.env.GITHUB_REF_TYPE as string
+    this.refProtected = new Boolean(process.env.GITHUB_REF_PROTECTED as string)
     this.workflow = process.env.GITHUB_WORKFLOW as string
     this.action = process.env.GITHUB_ACTION as string
     this.actor = process.env.GITHUB_ACTOR as string
